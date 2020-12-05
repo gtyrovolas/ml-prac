b0ca3999f81cea8214e2609c8d2d1a4100022927
Giannis Tyrovolas <johntyro@hotmail.com>
2020-12-05 22:55:23 +0000

    extra refactoring

--- main.py
+++ main.py
@@ -12,6 +12,22 @@ pp = pprint.PrettyPrinter(indent=4)
 def  meanSqErr(y, y_hat):
 	return np.square(y - y_hat).mean() / 2
 
+# makes a line graph for the two error lines
+def mkLineGraph(errors, step):
+	x = range(20, 600, step)
+	trainErrs = [error[0] for error in errors]
+	testErrs =[error[1] for error in errors]
+
+	plt.plot(x, trainErrs, color='skyblue', linewidth=1.5, label = "Training error")
+	plt.plot(x, testErrs, color='red', linewidth=1.5, label = "Testing error")
+	plt.xlabel('Elements used for training')
+	plt.ylabel('Error')
+	plt.title('Testing vs Training error for different values of n_train')
+	plt.legend()
+
+	plt.show()
+	
+
 # constructs and shows a bar graph of the distribution of wine quality
 def mkbar(y):
 	qual_cnt = [0] * 10
@@ -49,13 +65,11 @@ def lin(trainingData, testingData):
 	X_train, y_train, n_train = trainingData
 	X_test, y_test, n_test = testingData
 
-	# regularise training and testing data
-	regularise(X_train, X_test)
-
 	# add a bias term
 	ones_vector_train = [[1] for i in range(n_train)]
 	ones_vector_test = [[1] for i in range(n_test)]
 	
+	# adjoin ones vector
 	X_train = np.hstack((ones_vector_train, X_train))
 	X_test = np.hstack((ones_vector_test, X_test))
 
@@ -78,24 +92,12 @@ def split(X, y, n_train, n_test):
 
 	return X_train, y_train, X_test, y_test
 
-# wrapper for linear regression
+# wrapper for regularised linear regression
 def wrapLin(X, y, n_train, n_test):
 	X_train, y_train, X_test, y_test = split(X.copy(), y, n_train, n_test)
+	regularise(X_train, X_test)
 	return lin((X_train, y_train, n_train), (X_test, y_test, n_test))
 
-def visualiseErrors(errors, step):
-	x = range(20, 600, step)
-	trainErrs = [error[0] for error in errors]
-	testErrs =[error[1] for error in errors]
-
-	plt.plot(x, trainErrs, color='skyblue', linewidth=1.5, label = "Training error")
-	plt.plot(x, testErrs, color='red', linewidth=1.5, label = "Testing error")
-	plt.xlabel('Elements used for training')
-	plt.ylabel('Error')
-	plt.title('Testing vs Training error for different values of n_train')
-	plt.legend()
-	plt.show()
-	
 
 def main():
 
@@ -119,7 +121,7 @@ def main():
 #	Handin 4: Overfitting graphs
 	step = 20
 	errors = [wrapLin(X, y, n_train, n - n_train) for n_train in range(20, 600, step)]
-	visualiseErrors(errors, step)
+	mkLineGraph(errors, step)
 
 #	The difference of training error and testing error is sufficiently small for n >= 500
 #	I don't think we need more training data. Nevertheless, the model is underfitting due
