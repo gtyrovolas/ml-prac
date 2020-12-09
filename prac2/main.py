7290245a6efe6851691f395c25e1f5433257513c
Giannis Tyrovolas <johntyro@hotmail.com>
2020-12-09 03:49:01 +0200

    skeleton code

---
+++ prac2/main.py
@@ -0,0 +1,24 @@
+
+
+class NBC:
+
+    feature_types = []
+    num_classes = 0
+
+    def __init__(self, feature_types, num_classes):
+        self.feature_types = feature_types
+        self.num_classes = num_classes
+    
+    def fit(self, X, y):
+        """
+        Fit the right model to the data
+        """
+        pass
+
+    def predict(self, X):
+        """
+        Predict the outputs for input X
+        """
+        
+        pass
+
